6d435ef4739923474540f7cf5002054f9384aff8
Joel Silva <53149347+zoreu@users.noreply.github.com>
2021-03-07 00:02:10 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ sudo apt install gnupg ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-sudo apt install mono-devel
+sudo apt install mono-complete
 sudo apt-get -f install
 echo "###### Download webgrabplus #######"
 wget http://webgrabplus.com/sites/default/files/download/SW/V3.1.0/WebGrabPlus_V3.1_install.tar.gz
