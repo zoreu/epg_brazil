f166c68f5d0ccffa5332736c3bd0e7e883084dfe
Joel Silva <53149347+zoreu@users.noreply.github.com>
2021-03-07 00:18:07 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,12 @@ sudo apt-get install -y wget
 sudo apt-get install -y unzip
 echo "###### Preparando o webgrabplus #######"
 sudo apt update && upgrade
-sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-sudo apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu stable-bionic main
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates
+software-properties-common
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys
+ 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu
+ stable-bionic main'
 sudo apt update
 sudo apt install mono-complete
 echo "Verificando mono instalado"
